88c102d5d6b38f50ddbdc967cd7cb8975dcf41f1
Bartlomiej Stefanski <bartex00001@gmail.com>
2025-12-25 12:05:24 +0100

    [#13] Cpu.CPu: Define CPU flags

---
+++ src/Cpu/Cpu.hs
@@ -0,0 +1,36 @@
+module Cpu.Cpu where
+
+import Clash.Prelude
+
+import Utilities.Utils
+
+
+data ArithmeticFlags = ArithmeticFlags
+  { negative :: Active High
+  , overflow :: Active High
+  , decimal  :: Active High
+  , zero     :: Active High
+  , carry    :: Active High
+  } deriving (Eq, Show)
+
+defaultArithmeticFlags :: ArithmeticFlags
+defaultArithmeticFlags = ArithmeticFlags
+  { negative = toActive False
+  , overflow = toActive False
+  , decimal  = toActive False
+  , zero     = toActive False
+  , carry    = toActive False
+  }
+
+data CpuFlags = CpuFlags
+  { arithmeticFlags :: ArithmeticFlags
+  , brk :: Active High
+  , interrupt :: Active High
+  }
+
+defaultCpuFlags :: CpuFlags
+defaultCpuFlags = CpuFlags
+  { arithmeticFlags = defaultArithmeticFlags
+  , brk = toActive False
+  , interrupt = toActive False
+  }

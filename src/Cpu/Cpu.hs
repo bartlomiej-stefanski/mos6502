73487e555f67d1197ae2f012101cd1a99d687b92
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-01-30 14:21:51 +0100

    [DEV] cpu.cpu: Add splitAddr

--- src/Cpu/Cpu.hs
+++ src/Cpu/Cpu.hs
@@ -5,3 +5,9 @@ import Clash.Prelude
 type Data = Unsigned 8
 
 type Addr = Unsigned 16
+
+splitAddr :: Addr -> (Data, Data)
+splitAddr addr = (highByte, lowByte)
+  where
+    highByte = bitCoerce $ slice d15 d8 addr :: Data
+    lowByte = bitCoerce $ slice d7 d0 addr :: Data

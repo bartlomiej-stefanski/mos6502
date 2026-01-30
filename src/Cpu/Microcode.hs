e9a8ca472a690af058b3b6f0cb7b69b8af01b796
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-01-30 14:25:48 +0100

    [#5] cpu.Microcode: Rewrite bus access operations

--- src/Cpu/Microcode.hs
+++ src/Cpu/Microcode.hs
@@ -1,32 +1,51 @@
 module Cpu.Microcode where
 
 import Clash.Prelude
+import Cpu.Instructions (AddressingMode, Instruction)
 
 data MicroCmd
   = -- | Execute actual instruction.
     CmdExecute
   | -- | Decode opcode on bus to uncover next MicroOP.
+    -- This is the last instruction in a chain.
     CmdDecodeOpcode
   | -- | Do not perform operations.
     CmdNOP
   deriving (Eq, Show, Generic, NFDataX)
 
-data BusAddress = PC | SP | COMPUTE_FROM_MODE
+data BusAddressOffset = NONE | REGX | REGY
   deriving (Eq, Show, Generic, NFDataX)
 
-data BusDataSource
-  = DATA_SOURCE_PC_LOW
-  | DATA_SOURCE_PC_HIGH
-  | DATA_SOURCE_STATUS
-  | DATA_SOURCE_ALU
+data BusAddress
+  = SP
+  | PC
+  | BUS_VALUE
+  | DATA_LATCH_AND_BUS
+  deriving (Eq, Show, Generic, NFDataX)
+
+data BusDataSourceWrite
+  = DATA_WRITE_PC_LOW
+  | DATA_WRITE_PC_HIGH
+  | DATA_WRITE_STATUS
+  | DATA_WRITE_ALU
+  deriving (Eq, Show, Generic, NFDataX)
+
+data BusDataSourceRead
+  = DATA_READ_PC_LOW
+  | DATA_READ_PC_HIGH
+  | DATA_READ_STATUS
+  | DATA_READ
   deriving (Eq, Show, Generic, NFDataX)
 
 data BusOP
   = BusOP
   { -- | Some address must be specified; even if result will not be used.
     _address :: BusAddress,
-    -- | Data to write specified address. If empty treat as read.
-    _writeData :: Maybe BusDataSource
+    _addressOffset :: BusAddressOffset,
+    -- | Data to write onto bus. If empty treat as read request.
+    _writeData :: Maybe BusDataSourceWrite,
+    -- | What happens with data on bus.
+    _readData :: Maybe BusDataSourceRead
   }
   deriving (Eq, Show, Generic, NFDataX)
 
@@ -36,3 +55,8 @@ data MicroOP
     _busOp :: BusOP
   }
   deriving (Eq, Show, Generic, NFDataX)
+
+type MicroOpIndex = Index 1024
+
+getNextMicrocodeIndex :: (Instruction, AddressingMode) -> MicroOpIndex
+getNextMicrocodeIndex = errorX "Not Implemented"

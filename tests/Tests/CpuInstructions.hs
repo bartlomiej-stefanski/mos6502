54a01e6bff6c01a0e77beb670db3e2b940a1add2
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-02-04 14:31:13 +0100

    [#6] tests: Add instruction execution tests

--- tests/Tests/CpuInstructions.hs
+++ tests/Tests/CpuInstructions.hs
@@ -1,26 +1,59 @@
+{-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
 module Tests.CpuInstructions where
 
 import Clash.Prelude
+import Cpu.Cpu
+import Cpu.CpuState
+import Cpu.Data
 import Cpu.Instructions
+import Cpu.Microcode.Map
 import qualified Hedgehog as H
 import Test.Tasty
 import Test.Tasty.Hedgehog
 import Test.Tasty.TH
 import Tests.CpuGenerator
 
--- | Checks whether 'decode' can be executed for all possible instructions.
--- Result of decode is verified only to force evaluation.
-prop_should_decode_all_instructions :: H.Property
-prop_should_decode_all_instructions = H.property $ do
-  opcode <- H.forAll genOpcode
-  let instruction = fst $ decode opcode
-  -- Just ensure that decode does not crash for any instruction.
-  if opcode == 0xEA
-    then instruction H.=== NOP
-    else NOP H./== instruction
+prop_reset_works :: H.Property
+prop_reset_works = H.property $ do
+  resetPC <- H.forAll genAddr
 
-decodeCoverageTests :: TestTree
-decodeCoverageTests = $(testGroupGenerator)
+  let (pcHigh, pcLow) = splitAddr resetPC
+
+  let op1 : op2 : op3 : _ = microcodeInstructions (JMP, Absolute None)
+
+  let (syncState, _) = cpuWithBus initCpuStateWithBus (0, op1)
+
+  syncState
+    H.=== initCpuStateWithBus
+      { _cpuState =
+          (_cpuState initCpuStateWithBus)
+            { _sync_after_reset = False
+            }
+      }
+
+  let (stateWithBus, directBus) = cpuWithBus syncState (0, op1)
+
+  _regPC (_cpuState stateWithBus) H.=== resetVector + 1
+  _addressToQuery directBus H.=== resetVector
+
+  let oldStateWithBus = stateWithBus
+  let (stateWithBus, directBus) = cpuWithBus oldStateWithBus (pcLow, op2)
+
+  _regPC (_cpuState stateWithBus) H.=== resetVector + 2
+  _addressToQuery directBus H.=== resetVector + 1
+  _dataLatch (_cpuState stateWithBus) H.=== pcLow
+
+  let oldStateWithBus = stateWithBus
+  let (stateWithBus, directBus) = cpuWithBus oldStateWithBus (pcHigh, op3)
+
+  _regPC (_cpuState stateWithBus) H.=== resetPC + 1
+  _addressToQuery directBus H.=== resetPC
+  _dataLatch (_cpuState stateWithBus) H.=== pcHigh
+
+cpuInstructionTests :: TestTree
+cpuInstructionTests = $(testGroupGenerator)
 
 main :: IO ()
-main = defaultMain decodeCoverageTests
+main = defaultMain cpuInstructionTests
--- tests/unittests.hs
+++ tests/unittests.hs
@@ -1,6 +1,7 @@
 import Test.Tasty
 import Test.Tasty.Hedgehog
 import qualified Tests.Alu
+import qualified Tests.CpuDecoder
 import qualified Tests.CpuExecutor
 import qualified Tests.CpuInstructions
 import qualified Tests.SanityCheck
@@ -12,9 +13,10 @@ main =
     testGroup
       "."
       [ testTreeLimit 1 Tests.SanityCheck.amISane,
-        testTreeLimit 1000 Tests.CpuInstructions.decodeCoverageTests,
         testTreeLimit 10000 Tests.Alu.aluTests,
-        testTreeLimit 100000 Tests.CpuExecutor.cpuExecutorTests
+        testTreeLimit 1000 Tests.CpuDecoder.decodeCoverageTests,
+        testTreeLimit 100000 Tests.CpuExecutor.cpuExecutorTests,
+        testTreeLimit 10000 Tests.CpuInstructions.cpuInstructionTests
       ]
   where
     testTreeLimit n = localOption (HedgehogTestLimit (Just n))

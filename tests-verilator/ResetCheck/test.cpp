2f145682a7f044661bfa1ffaf9674ed72aad6295
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-02-04 14:56:53 +0100

    [#6] test-verilator: Add full reset tests

--- tests-verilator/ResetCheck/test.cpp
+++ tests-verilator/ResetCheck/test.cpp
@@ -3,23 +3,124 @@
 #include <VtopEntity.h>
 
 #include "Common.hpp"
+#include "CpuTest.hpp"
+#include "Instructions.hpp"
 
-TEST(ResetCheck, InitialStateIsCorrect)
+constexpr Addr program_start{0x8090};
+
+class ResetCheckTests : public CpuTest
 {
-  VtopEntity cpu;
-  cpu.ENABLE = 1;
-  cpu.RESET = 0;
-  cpu.CLK = 0;
-  cpu.eval();
-  cpu.RESET = 1;
-  cpu.CLK = 1;
-  cpu.eval();
+protected:
+  void SetUpMemory() override
+  {
+    CpuTest::SetUpMemory();
 
-  EXPECT_EQ(cpu.REG_A, 0);
-  EXPECT_EQ(cpu.REG_X, 0);
-  EXPECT_EQ(cpu.REG_Y, 0);
-  EXPECT_EQ(cpu.SP, 0xFF);
-  EXPECT_EQ(cpu.PC, ResetVector + 1);
-  EXPECT_EQ(cpu.MEM_ADDR, ResetVector);
-  EXPECT_EQ(cpu.MEM_W, false);
+    memory_maps.insert({
+      ResetVector,
+      MemoryLayer(
+        "ResetVector",
+        {MO(program_start)}
+      )
+    });
+
+    memory_maps.insert({
+      program_start,
+      MemoryLayer(
+        "Program Memory",
+        {
+          MO(Instruction::nop()),
+          MO(Instruction::nop()),
+          MO(Instruction::nop())
+        }
+      )
+    });
+  }
+};
+
+TEST_F(ResetCheckTests, InitialStateIsCorrect)
+{
+  for (auto i{0}; i < 50; i++) {
+    cpu->RESET = 1;
+    tick();
+
+    EXPECT_EQ(cpu->REG_A, 0);
+    EXPECT_EQ(cpu->REG_X, 0);
+    EXPECT_EQ(cpu->REG_Y, 0);
+    EXPECT_EQ(cpu->SP, 0xFF);
+    EXPECT_EQ(cpu->PC, ResetVector);
+    EXPECT_EQ(cpu->MEM_ADDR, 0);
+    EXPECT_EQ(cpu->MEM_W, false);
+  }
+}
+
+TEST_F(ResetCheckTests, InitialOperationsAreCorrect)
+{
+  cpu->RESET = 1;
+  tick();
+  cpu->RESET = 0;
+  tick();
+
+  {
+    SCOPED_TRACE("Booting");
+    expect_regs_change({.pc = ResetVector});
+  }
+
+  tick();
+
+  {
+    SCOPED_TRACE("Request LOW");
+    expect_regs_change({.pc = ResetVector + 1});
+    expect_bus_read(ResetVector);
+  }
+
+  tick();
+
+  {
+    SCOPED_TRACE("Request High");
+    expect_regs_change({.pc = ResetVector + 2});
+    expect_bus_read(ResetVector + 1);
+  }
+
+  tick();
+
+  {
+    SCOPED_TRACE("Read first program opcode");
+    expect_regs_change({.pc = program_start + 1});
+    expect_bus_read(program_start);
+  }
+
+  tick();
+
+  {
+    SCOPED_TRACE("Decode NOP opcode");
+    expect_regs_change({.pc = program_start + 1});
+    expect_bus_read(program_start);
+  }
+
+  tick();
+
+  {
+    SCOPED_TRACE("Execute NOP: just ask for next opcode");
+    expect_regs_change({.pc = program_start + 2});
+    expect_bus_read(program_start + 1);
+  }
+}
+
+TEST_F(ResetCheckTests, ResetBringsBackInitialState)
+{
+  reset();
+
+  tick(5);
+
+  EXPECT_EQ(cpu->PC, program_start + 2);
+
+  reset();
+
+  EXPECT_EQ(cpu->REG_A, 0);
+  EXPECT_EQ(cpu->REG_X, 0);
+  EXPECT_EQ(cpu->REG_Y, 0);
+  EXPECT_EQ(cpu->SP, 0xFF);
+  EXPECT_EQ(cpu->PC, ResetVector);
+  EXPECT_EQ(cpu->MEM_ADDR, 0);
+  EXPECT_EQ(cpu->MEM_W, false);
 }

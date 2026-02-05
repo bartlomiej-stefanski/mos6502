025f2fa0df105663c1b7b13965c80350e40d1918
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-02-05 15:32:41 +0100

    [FIX] cpu.Instructions: Fix decoding of ST. instructions

--- tests/Tests/CpuInstructions.hs
+++ tests/Tests/CpuInstructions.hs
@@ -4,6 +4,7 @@
 module Tests.CpuInstructions where
 
 import Clash.Prelude
+import Cpu.Alu
 import Cpu.Cpu
 import Cpu.CpuState
 import Cpu.Data
@@ -51,6 +52,38 @@ prop_reset_works = H.property $ do
   _regPC (_cpuState stateWithBus) H.=== resetPC + 1
   _addressToQuery directBus H.=== resetPC
   _dataLatch (_cpuState stateWithBus) H.=== pcHigh
+
+prop_xor_works :: H.Property
+prop_xor_works = H.property $ do
+  regA <- H.forAll genData
+  immediate <- H.forAll genData
+  regPC <- H.forAll genAddr
+
+  let xorImmediate = Compute (BinaryOp XOR) ALUConnect {_left = Just RegA, _right = Memory, _output = Just RegA} True
+  let op1 : op2 : _ = microcodeInstructions (xorImmediate, Immediate)
+
+  let initState =
+        initCpuStateWithBus
+          { _cpuState =
+              initCpuState
+                { _regPC = regPC,
+                  _regA = regA,
+                  _sync_after_reset = False,
+                  _instruction = xorImmediate
+                }
+          }
+
+  let (stateWithBus, directBus) = cpuWithBus initState (immediate, op1)
+
+  _regPC (_cpuState stateWithBus) H.=== regPC + 1
+  _addressToQuery directBus H.=== regPC
+
+  let oldStateWithBus = stateWithBus
+  let (stateWithBus, directBus) = cpuWithBus oldStateWithBus (immediate, op2)
+
+  _regPC (_cpuState stateWithBus) H.=== regPC + 2
+  _addressToQuery directBus H.=== regPC + 1
+  _regA (_cpuState stateWithBus) H.=== regA `xor` immediate
 
 cpuInstructionTests :: TestTree
 cpuInstructionTests = $(testGroupGenerator)

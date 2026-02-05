c01789904bf6066c06a03f3cc2a2169d00f9ff09
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-02-05 15:34:36 +0100

    [#6] test-verilator: Add flag-testing

--- tests-verilator/CpuTest.cpp
+++ tests-verilator/CpuTest.cpp
@@ -116,6 +116,16 @@ void CpuTest::tick()
     .sp = cpu->SP
   };
 
+  prev_flags = {
+    .carry = cpu->CARRY_AF,
+    .zero = cpu->ZERO_AF,
+    .interrupt_disable = cpu->INT_F,
+    .decimal_mode = cpu->DEC_AF,
+    .break_command = cpu->BRK_F,
+    .overflow = cpu->OVF_AF,
+    .negative = cpu->NEG_AF
+  };
+
   cpu->CLK = 0;
   cpu->eval();
 
@@ -160,6 +170,29 @@ void CpuTest::expect_regs_change(RegsState expected)
   CHECK_IF_CHANGE_EXPECTED(x, REG_X)
   CHECK_IF_CHANGE_EXPECTED(y, REG_Y)
   CHECK_IF_CHANGE_EXPECTED(sp, SP)
+
+#undef CHECK_IF_CHANGE_EXPECTED
+}
+
+void CpuTest::expect_flags_change(CpuFlagState expected)
+{
+  SCOPED_TRACE("EXPECT_FLAGS_CHANGE");
+
+#define CHECK_IF_CHANGE_EXPECTED(field, cpu_flag) \
+  if (expected.field.has_value()) { \
+    EXPECT_EQ(cpu->cpu_flag, *expected.field); \
+  } \
+  else { \
+    EXPECT_EQ(cpu->cpu_flag, *prev_flags.field); \
+  }
+
+  CHECK_IF_CHANGE_EXPECTED(carry, CARRY_AF);
+  CHECK_IF_CHANGE_EXPECTED(zero, ZERO_AF);
+  CHECK_IF_CHANGE_EXPECTED(interrupt_disable, INT_F);
+  CHECK_IF_CHANGE_EXPECTED(decimal_mode, DEC_AF);
+  CHECK_IF_CHANGE_EXPECTED(break_command, BRK_F);
+  CHECK_IF_CHANGE_EXPECTED(overflow, OVF_AF);
+  CHECK_IF_CHANGE_EXPECTED(negative, NEG_AF);
 
 #undef CHECK_IF_CHANGE_EXPECTED
 }
--- tests-verilator/CpuTest.hpp
+++ tests-verilator/CpuTest.hpp
@@ -44,6 +44,8 @@ protected:
   void TearDown() override;
 
   void reset();
+  static constexpr u64 ResetEntryCycles{5};
+  void reset_to_entry() { reset(); tick(ResetEntryCycles); }
 
   u8& get_memory(Addr addr, bool is_write);
 
@@ -67,7 +69,32 @@ protected:
     .sp = 0xFF
   };
 
+#define NEXT_PC (*prev_state.pc + 1)
+
+  struct CpuFlagState
+  {
+    std::optional< bool > carry{};
+    std::optional< bool > zero{};
+    std::optional< bool > interrupt_disable{};
+    std::optional< bool > decimal_mode{};
+    std::optional< bool > break_command{};
+    std::optional< bool > overflow{};
+    std::optional< bool > negative{};
+  };
+
+  CpuFlagState prev_flags{
+    .carry = false,
+    .zero = false,
+    .interrupt_disable = false,
+    .decimal_mode = false,
+    .break_command = false,
+    .overflow = false,
+    .negative = false
+  };
+
   void expect_regs_change(RegsState expected);
+  void expect_flags_change(CpuFlagState expected);
+
   void expect_bus_read(Addr bus_addr);
   void expect_bus_write(Addr bus_addr, u8 data);
 };

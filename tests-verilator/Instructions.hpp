9d91b5669fb3e6c582b21f1fc8ccacf605326ec6
Bartlomiej Stefanski <bartex00001@gmail.com>
2026-02-05 13:20:29 +0100

    [#6] test-verilator: Add tests for immediate instructions

--- tests-verilator/Instructions.hpp
+++ tests-verilator/Instructions.hpp
@@ -4,6 +4,24 @@
 
 #include "Common.hpp"
 
+enum class ImmediateOpcodes : u8
+{
+  LDA = 0xA9,
+  LDX = 0xA2,
+  LDY = 0xA0,
+
+  CMP = 0xC9,
+  CPY = 0xC0,
+  CPX = 0xE0,
+
+  ORA = 0x09,
+  AND = 0x29,
+  EOR = 0x49,
+
+  ADC = 0x69,
+  SBC = 0xE9,
+};
+
 class Instruction
 {
   Instruction() = default;
@@ -13,7 +31,7 @@ public:
 
   static Instruction nop();
 
-  static Instruction lda_immediate(u8 immediate);
+  static Instruction immediate(ImmediateOpcodes opcode, u8 immediate);
 
 private:
   std::vector< u8 > data;
